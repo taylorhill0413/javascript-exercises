631a22fad1f4275794978869362220347bfeed58
Taylor Hill <taylorhill0413@gmail.com>
2025-06-01 14:23:56 +0700

    using array

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0)
+        return "OOPS"
+    num = Number(num)
+    var arr = new Array(1,1);
+    var j = 0;
+    if (num == 0)
+        return 0
+    if (num < 2)
+        return arr[num-1]
+    for (var i=2; i < num; i++){
+        j = arr[i-1]+arr[i-2]
+        arr.push(j)
+    }
+    return arr[num-1]
 };
 
 // Do not edit below this line
